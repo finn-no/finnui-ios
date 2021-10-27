272b248482d84a4cf1727e7cf8e5e5ad0ec7dfa7
Bastian Stien <bastian.stien@gmail.com>
2021-10-27 12:58:48 +0200

    Add Colors type

--- Sources/Components/RealestateAgencyContent/RealestateAgencyContentViewModel.swift
+++ Sources/Components/RealestateAgencyContent/RealestateAgencyContentViewModel.swift
@@ -3,12 +3,16 @@ import UIKit
 public struct RealestateAgencyContentViewModel {
     public let logoUrl: String
     public let articles: [ArticleItem]
+    public let colors: Colors
 
-    public init(logoUrl: String, articles: [ArticleItem]) {
+    public init(logoUrl: String, articles: [ArticleItem], colors: Colors) {
         self.logoUrl = logoUrl
         self.articles = articles
+        self.colors = colors
     }
 }
+
+// MARK: - ArticleItem
 
 extension RealestateAgencyContentViewModel {
     public struct ArticleItem {
@@ -29,6 +33,30 @@ extension RealestateAgencyContentViewModel {
             self.imageUrl = imageUrl
             self.buttonTitle = buttonTitle
             self.buttonKind = buttonKind
+        }
+    }
+}
+
+// MARK: - Colors
+
+extension RealestateAgencyContentViewModel {
+    public struct Colors {
+        public let main: Group
+        public let actionButton: Group
+
+        public init(main: Group, actionButton: Group) {
+            self.main = main
+            self.actionButton = actionButton
+        }
+
+        public struct Group {
+            public let text: UIColor
+            public let background: UIColor
+
+            public init(text: UIColor, background: UIColor) {
+                self.text = text
+                self.background = background
+            }
         }
     }
 }
