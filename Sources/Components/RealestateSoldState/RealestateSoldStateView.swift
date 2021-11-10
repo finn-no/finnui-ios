1a6b05a4cdc5c7f2213319f454d2dde00bb574e0
Bastian Stien <bastian.stien@gmail.com>
2021-11-10 12:46:09 +0100

    Add initial RealestateSoldStateView

---
+++ Sources/Components/RealestateSoldState/RealestateSoldStateView.swift
@@ -0,0 +1,49 @@
+import UIKit
+import FinniversKit
+
+public class RealestateSoldStateView: UIView {
+
+    // MARK: - Public properties
+
+    // MARK: - Private properties
+
+    private lazy var stackView = UIStackView(axis: .vertical, spacing: .spacingL, withAutoLayout: true)
+
+    private lazy var questionFormView: QuestionFormView = {
+        let view = QuestionFormView(withAutoLayout: true)
+        view.delegate = self
+        view.configure(with: "Hva lurer du på?", questions: [
+            .init(kind: .provided, title: "Hva ble boligen solgt for?", isSelected: true),
+            .init(kind: .provided, title: "Hvor mange var det som kom på visning?", isSelected: false),
+            .init(kind: .provided, title: "Hva kan man forvente av en budrunde?", isSelected: false),
+            .init(kind: .provided, title: "Kan jeg få en verdivurdering av min bolig?", isSelected: false),
+            .init(kind: .userFreetext, title: "Annet", isSelected: true, value: "Long text\nSeveral lines\nAnother line"),
+        ])
+        return view
+    }()
+
+    // MARK: - Init
+
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+
+    public required init?(coder: NSCoder) { fatalError() }
+
+    // MARK: - Setup
+
+    private func setup() {
+        stackView.addArrangedSubviews([questionFormView])
+        addSubview(stackView)
+        stackView.fillInSuperview(insets: UIEdgeInsets(top: 0, left: 16, bottom: 0, right: -16))
+    }
+}
+
+// MARK: - QuestionFormViewDelegate
+
+extension RealestateSoldStateView: QuestionFormViewDelegate {
+    func questionFormViewDidToggleTextView(_ view: QuestionFormView) {
+        print("✒️ Did toggle textView")
+    }
+}
