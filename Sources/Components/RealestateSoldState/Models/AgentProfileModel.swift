e708f83b32b058420d34c1ac5f4d0e9d923610f6
Bastian Stien <bastian.stien@gmail.com>
2021-11-11 14:55:30 +0100

    Add AgentProfileView + model

---
+++ Sources/Components/RealestateSoldState/Models/AgentProfileModel.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+public struct AgentProfileModel {
+    public let title: String
+    public let agentName: String
+    public let agentJobTitle: String
+    public let imageUrl: String
+    public let phoneNumber: String?
+    public let email: String?
+
+    public init(title: String, agentName: String, agentJobTitle: String, imageUrl: String, phoneNumber: String?, email: String?) {
+        self.title = title
+        self.agentName = agentName
+        self.agentJobTitle = agentJobTitle
+        self.imageUrl = imageUrl
+        self.phoneNumber = phoneNumber
+        self.email = email
+    }
+}
