77b75a987b17ea911ca3c7a377b250f99f2fb0b8
Bastian Stien <bastian.stien@gmail.com>
2021-11-16 14:46:08 +0100

    Add CompanyProfileView to RealestateSoldStateView

--- Sources/Components/RealestateSoldState/RealestateSoldStateView.swift
+++ Sources/Components/RealestateSoldState/RealestateSoldStateView.swift
@@ -3,6 +3,8 @@ import FinniversKit
 
 public protocol RealestateSoldStateViewDelegate: AnyObject {
     func realestateSoldStateView(_ view: RealestateSoldStateView, didSubmitForm form: RealestateSoldStateQuestionFormSubmit)
+    func realestateSoldStateViewDidSelectCompanyProfileCtaButton(_ view: RealestateSoldStateView)
+    func realestateSoldStateView(_ view: RealestateSoldStateView, didTapCompanyProfileButtonWithIdentifier identifier: String?, url: URL)
 }
 
 public class RealestateSoldStateView: UIView {
@@ -16,6 +18,7 @@ public class RealestateSoldStateView: UIView {
     private lazy var stackView = UIStackView(axis: .vertical, spacing: .spacingM, withAutoLayout: true)
     private lazy var questionFormView = QuestionFormContainerView(delegate: self, withAutoLayout: true)
     private lazy var agentProfileView = AgentProfileView(withAutoLayout: true)
+    private lazy var companyProfileView = CompanyProfileView(delegate: self, withAutoLayout: true)
 
     // MARK: - Init
 
@@ -29,7 +32,7 @@ public class RealestateSoldStateView: UIView {
     // MARK: - Setup
 
     private func setup() {
-        stackView.addArrangedSubviews([questionFormView, agentProfileView])
+        stackView.addArrangedSubviews([questionFormView, agentProfileView, companyProfileView])
         addSubview(stackView)
         stackView.fillInSuperview(insets: UIEdgeInsets(top: 0, left: .spacingM, bottom: 0, right: -.spacingM))
     }
@@ -39,6 +42,7 @@ public class RealestateSoldStateView: UIView {
     public func configure(with viewModel: RealestateSoldStateModel, remoteImageViewDataSource: RemoteImageViewDataSource) {
         questionFormView.configure(with: viewModel.questionForm)
         agentProfileView.configure(with: viewModel.agentProfile, remoteImageViewDataSource: remoteImageViewDataSource)
+        companyProfileView.configure(with: viewModel.companyProfile, remoteImageViewDataSource: remoteImageViewDataSource)
     }
 }
 
@@ -47,5 +51,17 @@ public class RealestateSoldStateView: UIView {
 extension RealestateSoldStateView: QuestionFormContainerViewDelegate {
     func questionFormContainerView(_ view: QuestionFormContainerView, didSubmitForm form: RealestateSoldStateQuestionFormSubmit) {
         delegate?.realestateSoldStateView(self, didSubmitForm: form)
+    }
+}
+
+// MARK: - CompanyProfileViewDelegate
+
+extension RealestateSoldStateView: CompanyProfileViewDelegate {
+    func companyProfileViewDidSelectCtaButton(_ view: CompanyProfileView) {
+        delegate?.realestateSoldStateViewDidSelectCompanyProfileCtaButton(self)
+    }
+
+    func companyProfileView(_ view: CompanyProfileView, didTapButtonWithIdentifier identifier: String?, url: URL) {
+        delegate?.realestateSoldStateView(self, didTapCompanyProfileButtonWithIdentifier: identifier, url: url)
     }
 }
