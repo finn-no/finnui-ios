da1a2ed3c43e0e57aa7e5b103e7de85265d19793
Bastian Stien <bastian.stien@gmail.com>
2021-11-17 10:11:45 +0100

    Realestate sold state: (4) Company profile (#36)

--- Demo/Sources/Components/RealestateSoldState/RealestateSoldStateDemoView.swift
+++ Demo/Sources/Components/RealestateSoldState/RealestateSoldStateDemoView.swift
@@ -47,6 +47,11 @@ extension RealestateSoldStateDemoView: RemoteImageViewDataSource {
     }
 
     func remoteImageView(_ view: RemoteImageView, loadImageWithPath imagePath: String, imageWidth: CGFloat, completion: @escaping (UIImage?) -> Void) {
+        if imagePath == "FINN-LOGO" {
+            completion(UIImage(named: .finnLogoLarge))
+            return
+        }
+
         guard let url = URL(string: imagePath) else {
             completion(nil)
             return
@@ -82,6 +87,14 @@ extension RealestateSoldStateDemoView: RealestateSoldStateViewDelegate {
               \t\t\(form.questions.joined(separator: "\n\t\t"))
               """)
     }
+
+    func realestateSoldStateViewDidSelectCompanyProfileCtaButton(_ view: RealestateSoldStateView) {
+        print("👉 Did select company profile CTA button.")
+    }
+
+    func realestateSoldStateView(_ view: RealestateSoldStateView, didTapCompanyProfileButtonWithIdentifier identifier: String?, url: URL) {
+        print("👉 Did select company profile button with identifier '\(identifier ?? "??")'")
+    }
 }
 
 // MARK: - Private extensions
@@ -90,7 +103,8 @@ private extension RealestateSoldStateModel {
     static var demoModel: RealestateSoldStateModel {
         RealestateSoldStateModel(
             agentProfile: .demoModel,
-            questionForm: .demoModel
+            questionForm: .demoModel,
+            companyProfile: .demoModel
         )
     }
 }
@@ -135,6 +149,38 @@ private extension QuestionFormViewModel {
             ],
             submitDisclaimer: "Ved å trykke \"Send skjema\" samtykker du til at FINN kan sende dine opplysninger fra skjema over til ansvarlig megler for denne annonsen.",
             submitButtonTitle: "Send skjema"
+        )
+    }
+}
+
+extension CompanyProfileModel {
+    static var demoModel: CompanyProfileModel {
+        CompanyProfileModel(
+            imageUrl: "FINN-LOGO",
+            slogan: "Vi selger boligen din – raskt og enkelt",
+            buttonLinks: [
+                .init(identifier: "1", title: "Hjemmeside"),
+                .init(identifier: "2", title: "Flere annonser fra oss"),
+                .init(identifier: "3", title: "Skal du selge bolig?"),
+            ],
+            ctaButtonTitle: "Be om verdivurdering"
+        )
+    }
+}
+
+private extension LinkButtonViewModel {
+    private static var demoStyle: Button.Style {
+        Button.Style.flat.overrideStyle(margins: UIEdgeInsets(vertical: .spacingS, horizontal: .zero), smallFont: .body)
+    }
+
+    init(identifier: String, title: String) {
+        self.init(
+            buttonIdentifier: identifier,
+            buttonTitle: title,
+            linkUrl: URL(string: "https://finn.no")!,
+            isExternal: false,
+            buttonStyle: Self.demoStyle,
+            buttonSize: .small
         )
     }
 }
--- Sources/Components/RealestateSoldState/Models/RealestateSoldStateModel.swift
+++ Sources/Components/RealestateSoldState/Models/RealestateSoldStateModel.swift
@@ -3,9 +3,15 @@ import Foundation
 public struct RealestateSoldStateModel {
     public let agentProfile: AgentProfileModel
     public let questionForm: QuestionFormViewModel
+    public let companyProfile: CompanyProfileModel
 
-    public init(agentProfile: AgentProfileModel, questionForm: QuestionFormViewModel) {
+    public init(
+        agentProfile: AgentProfileModel,
+        questionForm: QuestionFormViewModel,
+        companyProfile: CompanyProfileModel
+    ) {
         self.agentProfile = agentProfile
         self.questionForm = questionForm
+        self.companyProfile = companyProfile
     }
 }
