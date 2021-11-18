868afbe1434d807a1b63b1664570aa7354f607ef
Bastian Stien <bastian.stien@gmail.com>
2021-11-18 12:53:59 +0100

    Add model RealestateSoldStateModel.Styling

--- Sources/Components/RealestateSoldState/Models/RealestateSoldStateModel.swift
+++ Sources/Components/RealestateSoldState/Models/RealestateSoldStateModel.swift
@@ -4,14 +4,17 @@ public struct RealestateSoldStateModel {
     public let agentProfile: AgentProfileModel
     public let questionForm: QuestionFormViewModel
     public let companyProfile: CompanyProfileModel
+    public let styling: Styling
 
     public init(
         agentProfile: AgentProfileModel,
         questionForm: QuestionFormViewModel,
-        companyProfile: CompanyProfileModel
+        companyProfile: CompanyProfileModel,
+        styling: Styling
     ) {
         self.agentProfile = agentProfile
         self.questionForm = questionForm
         self.companyProfile = companyProfile
+        self.styling = styling
     }
 }
