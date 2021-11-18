94c9626b382bf2c71d5479dafc272a5a4b41095a
Bastian Stien <bastian.stien@gmail.com>
2021-11-18 11:23:55 +0100

    Pull contactMethod into separate model

--- Sources/Components/RealestateSoldState/Models/QuestionFormViewModel.swift
+++ Sources/Components/RealestateSoldState/Models/QuestionFormViewModel.swift
@@ -3,27 +3,33 @@ import Foundation
 public struct QuestionFormViewModel {
     public let questionsTitle: String
     public let questions: [RealestateSoldStateQuestionModel]
-    public let contactMethodTitle: String
-    public let contactMethodEmail: UserContactMethodSelectionModel.Email
-    public let contactMethodPhone: UserContactMethodSelectionModel.Phone
+    public let contactMethod: ContactMethod?
     public let submitDisclaimer: String
     public let submitButtonTitle: String
 
     public init(
         questionsTitle: String,
         questions: [RealestateSoldStateQuestionModel],
-        contactMethodTitle: String,
-        contactMethodEmail: UserContactMethodSelectionModel.Email,
-        contactMethodPhone: UserContactMethodSelectionModel.Phone,
+        contactMethod: QuestionFormViewModel.ContactMethod?,
         submitDisclaimer: String,
         submitButtonTitle: String
     ) {
         self.questionsTitle = questionsTitle
         self.questions = questions
-        self.contactMethodTitle = contactMethodTitle
-        self.contactMethodEmail = contactMethodEmail
-        self.contactMethodPhone = contactMethodPhone
+        self.contactMethod = contactMethod
         self.submitDisclaimer = submitDisclaimer
         self.submitButtonTitle = submitButtonTitle
+    }
+
+    public struct ContactMethod {
+        public let title: String
+        public let emailMethod: UserContactMethodSelectionModel.Email
+        public let phoneMethod: UserContactMethodSelectionModel.Phone
+
+        public init(title: String, emailMethod: UserContactMethodSelectionModel.Email, phoneMethod: UserContactMethodSelectionModel.Phone) {
+            self.title = title
+            self.emailMethod = emailMethod
+            self.phoneMethod = phoneMethod
+        }
     }
 }
