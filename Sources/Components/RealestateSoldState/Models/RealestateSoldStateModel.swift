189b4fcac3a62055829d88a749b0afcc7216b1e4
Bastian Stien <bastian.stien@gmail.com>
2021-11-22 13:39:45 +0100

    Add logoUrl to RealestateSoldStateModel

--- Sources/Components/RealestateSoldState/Models/RealestateSoldStateModel.swift
+++ Sources/Components/RealestateSoldState/Models/RealestateSoldStateModel.swift
@@ -2,6 +2,7 @@ import Foundation
 
 public struct RealestateSoldStateModel {
     public let title: String
+    public let logoUrl: String
     public let agentProfile: AgentProfileModel
     public let questionForm: QuestionFormViewModel
     public let companyProfile: CompanyProfileModel
@@ -9,12 +10,14 @@ public struct RealestateSoldStateModel {
 
     public init(
         title: String,
+        logoUrl: String,
         agentProfile: AgentProfileModel,
         questionForm: QuestionFormViewModel,
         companyProfile: CompanyProfileModel,
         styling: Styling
     ) {
         self.title = title
+        self.logoUrl = logoUrl
         self.agentProfile = agentProfile
         self.questionForm = questionForm
         self.companyProfile = companyProfile
