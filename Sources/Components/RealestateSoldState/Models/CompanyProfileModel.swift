a6234b7c0a23fef764e2468da642f3d84d8953ea
Bastian Stien <bastian.stien@gmail.com>
2021-12-07 11:54:37 +0100

    Make slogan optional

--- Sources/Components/RealestateSoldState/Models/CompanyProfileModel.swift
+++ Sources/Components/RealestateSoldState/Models/CompanyProfileModel.swift
@@ -2,11 +2,11 @@ import FinniversKit
 
 public struct CompanyProfileModel {
     public let imageUrl: String
-    public let slogan: String
+    public let slogan: String?
     public let buttonLinks: [LinkButtonViewModel]
     public let ctaButtonTitle: String?
 
-    public init(imageUrl: String, slogan: String, buttonLinks: [LinkButtonViewModel], ctaButtonTitle: String?) {
+    public init(imageUrl: String, slogan: String?, buttonLinks: [LinkButtonViewModel], ctaButtonTitle: String?) {
         self.imageUrl = imageUrl
         self.slogan = slogan
         self.buttonLinks = buttonLinks
