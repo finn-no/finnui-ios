5b0e86139b066065748c7560f807f78bbc60c0ec
Bastian Stien <bastian.stien@gmail.com>
2021-12-21 10:13:27 +0100

    Bump FinniversKit to 88.0.2

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(name: "FinniversKit", url: "https://github.com/finn-no/FinniversKit.git", .exact("88.0.1")),
+        .package(name: "FinniversKit", url: "https://github.com/finn-no/FinniversKit.git", .exact("88.0.2")),
     ],
     targets: [
     	.target(
