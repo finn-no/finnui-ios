5c198c95b43708b9d62521750fe33f22bf238e31
Bastian Stien <bastian.stien@gmail.com>
2022-01-19 13:21:36 +0100

    Remove optional property phoneNumber and add non-optional phoneNumbers

--- Sources/Components/RealestateSoldState/Models/AgentProfileModel.swift
+++ Sources/Components/RealestateSoldState/Models/AgentProfileModel.swift
@@ -5,13 +5,13 @@ public struct AgentProfileModel {
     public let agentName: String
     public let agentJobTitle: String
     public let imageUrl: String?
-    public let phoneNumber: String?
+    public let phoneNumbers: [String]
 
-    public init(title: String, agentName: String, agentJobTitle: String, imageUrl: String?, phoneNumber: String?) {
+    public init(title: String, agentName: String, agentJobTitle: String, imageUrl: String?, phoneNumbers: [String]) {
         self.title = title
         self.agentName = agentName
         self.agentJobTitle = agentJobTitle
         self.imageUrl = imageUrl
-        self.phoneNumber = phoneNumber
+        self.phoneNumbers = phoneNumbers
     }
 }
