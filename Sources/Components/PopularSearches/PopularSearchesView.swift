921c1a1a7d44d4e6dc5fc2f98ff13a3cf19b92fa
Bastian Stien <bastian.stien@gmail.com>
2022-01-20 13:13:36 +0100

    Remove call to layoutIfNeeded – already handled by OverflowCollectionView

--- Sources/Components/PopularSearches/PopularSearchesView.swift
+++ Sources/Components/PopularSearches/PopularSearchesView.swift
@@ -47,7 +47,6 @@ public class PopularSearchesView: UIView {
 
     public func configure(with items: [String]) {
         overflowCollectionView.configure(with: items)
-        layoutIfNeeded()
     }
 }
 
