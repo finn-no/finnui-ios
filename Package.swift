da8a9e87bb618680f4e9af632adfa4771fb14b6c
Bastian Stien <bastian.stien@gmail.com>
2022-02-16 13:42:33 +0100

    ✅ Point to FinniversKit release

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(name: "FinniversKit", url: "https://github.com/finn-no/FinniversKit.git", .branch("component/OverflowCollectionView")),
+        .package(name: "FinniversKit", url: "https://github.com/finn-no/FinniversKit.git", .exact("90.4.0")),
     ],
     targets: [
     	.target(
