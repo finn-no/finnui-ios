d92f52536d97cfc3fa20776a11f42686be0acdf2
Attila Szilagyi <attila.szilagyi@schibsted.com>
2022-06-08 15:30:16 +0200

    Add TJTAccordionDemoView.

---
+++ Demo/Sources/Components/TJTAccordionDemoView/TJTAccordionDemoView.swift
@@ -0,0 +1,25 @@
+import FinniversKit
+import FinnUI
+
+final class TJTAccordionDemoView: UIView {
+    let accordionView = TJTAccordionView(withAutoLayout: true)
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+
+    public required init?(coder aDecoder: NSCoder) { fatalError() }
+
+    // MARK: - Setup
+
+    private func setup() {
+        addSubview(accordionView)
+
+        NSLayoutConstraint.activate([
+            accordionView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: .spacingM),
+            accordionView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: .spacingM),
+            accordionView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -.spacingM)
+        ])
+    }
+}
