8996a8d1c61af570c159b565219145cbfd154406
Attila Szilagyi <attila.szilagyi@schibsted.com>
2022-06-13 12:51:27 +0200

    Remove extraneous spacing.

--- Sources/Components/SuggestShippingView/ShippingRequestErrorView.swift
+++ Sources/Components/SuggestShippingView/ShippingRequestErrorView.swift
@@ -29,11 +29,11 @@ public final class ShippingRequestErrorView: UIView {
         setup()
         decorateWithViewModel()
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     private func setup() {
         layer.borderWidth = 1
         layer.borderColor = UIColor.sardine.cgColor
