abedb8f76884852dca0838f4adb9107f11da0b06
Attila Szilagyi <attila.szilagyi@schibsted.com>
2022-07-19 16:54:11 +0200

    Use development build for FinniversKit.

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(name: "FinniversKit", url: "https://github.com/finn-no/FinniversKit.git", .exact("93.6.0"))
+        .package(name: "FinniversKit", path: "../FinniversKit")
     ],
     targets: [
     	.target(
