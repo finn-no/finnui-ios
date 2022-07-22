ea0acc92dfa9bd4855d244cfaf3e8025e3da2032
Attila Szilagyi <attila.szilagyi@schibsted.com>
2022-07-22 19:28:12 +0200

    Add alert model to SuggestShippingViewModel.

--- Sources/Components/SuggestShippingView/SuggestShippingViewModel.swift
+++ Sources/Components/SuggestShippingView/SuggestShippingViewModel.swift
@@ -1,6 +1,7 @@
 import Combine
 
 public protocol SuggestShippingDelegate: AnyObject {
+    func didAskForConfirmation(with alertModel: AlertModel<Bool>) async -> Bool
     func didRequestShipping(forAdId adId: String)
 }
 
@@ -10,6 +11,7 @@ public class SuggestShippingViewModel {
     private(set) public var title: String
     private(set) public var message: String
     private(set) public var buttonTitle: String
+    private(set) public var alertModel: AlertModel<Bool>
 
     private let adId: String
     public weak var delegate: SuggestShippingDelegate?
@@ -18,16 +20,53 @@ public class SuggestShippingViewModel {
         title: String,
         message: String,
         buttonTitle: String,
-        adId: String
+        adId: String,
+        alertModel: AlertModel<Bool>
     ) {
         self.adId = adId
         self.title = title
         self.message = message
         self.buttonTitle = buttonTitle
+        self.alertModel = alertModel
     }
 
     public func suggestShipping() {
-        isProcessing = true
-        delegate?.didRequestShipping(forAdId: adId)
+        Task {
+            guard
+                await delegate?.didAskForConfirmation(with: alertModel) ?? false
+            else {
+                return
+            }
+
+            isProcessing = true
+            delegate?.didRequestShipping(forAdId: adId)
+        }
     }
 }
+
+public struct AlertModel<T> {
+    public struct ActionModel<T> {
+        public var title: String?
+        public var style: UIAlertAction.Style
+        public var value: T
+
+        public init(title: String? = nil, style: UIAlertAction.Style, value: T) {
+            self.title = title
+            self.style = style
+            self.value = value
+        }
+    }
+
+    public var actionModels = [ActionModel<T>]()
+    public var title: String?
+    public var message: String?
+    public var prefferedStyle: UIAlertController.Style
+
+    public init(actionModels: [AlertModel<T>.ActionModel<T>] = [ActionModel<T>](), title: String? = nil, message: String? = nil, prefferedStyle: UIAlertController.Style) {
+        self.actionModels = actionModels
+        self.title = title
+        self.message = message
+        self.prefferedStyle = prefferedStyle
+    }
+}
+
