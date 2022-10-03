b9737fa9971b73c94d7b66c91d52c6c0d5bd9421
Andrii <Omg0Mom@gmail.com>
2022-10-03 12:38:51 +0200

    Feature/apps 3779 slide model (#89)
    * Update Slide model
    
    * Fix Demo
    
    * APPS-3779: Add default values
    
    Co-authored-by: Andrii Momot <andrii.momot@schibsted.com>

--- Sources/Fullscreen/Stories/Models/StorySlideViewModel.swift
+++ Sources/Fullscreen/Stories/Models/StorySlideViewModel.swift
@@ -5,16 +5,25 @@ public struct StorySlideViewModel {
     public let title: String?
     public let detailText: String?
     public let price: String?
+    public let read: Bool?
+    public let notificationId: Int?
+    public let adId: Int?
 
     public init(
         imageUrl: String?,
         title: String?,
         detailText: String?,
-        price: String?
+        price: String?,
+        read: Bool?,
+        notificationId: Int? = nil,
+        adId: Int? = nil
     ) {
         self.imageUrl = imageUrl
         self.title = title
         self.detailText = detailText
         self.price = price
+        self.read = read
+        self.notificationId = notificationId
+        self.adId = adId
     }
 }
