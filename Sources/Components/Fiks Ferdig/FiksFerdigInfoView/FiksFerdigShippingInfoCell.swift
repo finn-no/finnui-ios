ec19f429b2d1b4bd32d9a6783f5da94c3c6c9fa1
Saad Qureshi <msaadq94@gmail.com>
2022-12-21 09:01:10 +0100

    Separate view model from the cell

--- Sources/Components/Fiks Ferdig/FiksFerdigInfoView/FiksFerdigShippingInfoCell.swift
+++ Sources/Components/Fiks Ferdig/FiksFerdigInfoView/FiksFerdigShippingInfoCell.swift
@@ -68,25 +68,3 @@ public final class FiksFerdigShippingInfoCell: UIStackView {
         ])
     }
 }
-
-public class FiksFerdigShippingInfoCellViewModel {
-    public enum ShippingProvider {
-        case heltHjem
-        case postnord
-        case posten
-    }
-
-    public let provider: ShippingProvider
-    public let providerName: String
-    public let message: String
-
-    public init(
-        provider: ShippingProvider,
-        providerName: String,
-        message: String
-    ) {
-        self.provider = provider
-        self.providerName = providerName
-        self.message = message
-    }
-}
