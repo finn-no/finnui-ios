a2fc74ae14121a22d21622a444f46c368842e920
Nina Røsdal Graneggen <ninagraneggen@gmail.com>
2023-01-23 14:47:38 +0100

    Add some more sections in preview

--- Sources/Fullscreen/SavedSearches/SavedSearchesViewModel+PreviewData.swift
+++ Sources/Fullscreen/SavedSearches/SavedSearchesViewModel+PreviewData.swift
@@ -12,6 +12,26 @@ extension SavedSearchesViewModel {
                     .init(title: "Fender Jaguar", text: "Kun på FINN.no"),
                     .init(title: "OP-1 Syntesizer", text: "På FINN.no, E-post og Push varsling")
                 ]
+            ),
+            SavedSearchesSection(
+                title: "Eiendom",
+                searches: [
+                    .init(title: "Valdresgata", text: "Ingen aktive varslinger"),
+                    .init(title: "Bolig til salgs - 'Strandlinje med Jacuzzi', Fra 250 m², Eiendom", text: "På FINN.no, E-post og Push varsling")
+                ]
+            ),
+            SavedSearchesSection(
+                title: "Jobb",
+                searches: [
+                    .init(title: "UX designer - Oslo", text: "Kun på FINN.no")
+                ]
+            ),
+            SavedSearchesSection(
+                title: "Bil",
+                searches: [
+                    .init(title: "Volvo", text: "Ingen aktive varsler"),
+                    .init(title: "Lastebil, fra 800 000 kr", text: "Ingen aktive varsler")
+                ]
             )
         ]
     )
