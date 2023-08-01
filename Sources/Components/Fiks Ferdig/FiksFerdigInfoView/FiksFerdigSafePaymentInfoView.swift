70a36aa334f1d7891ddbf453e399d77b3215a9fa
Bjørn Olav Ruud <bjorn.olav.ruud@schibsted.com>
2023-08-01 16:00:21 +0200

    Safe payment base view instead.

--- Sources/Components/Fiks Ferdig/FiksFerdigInfoView/FiksFerdigSafePaymentInfoView.swift
+++ Sources/Components/Fiks Ferdig/FiksFerdigInfoView/FiksFerdigSafePaymentInfoView.swift
@@ -2,24 +2,9 @@ import Combine
 import FinniversKit
 import UIKit
 
-public final class FiksFerdigSafePaymentInfoView: UIView {
+public final class FiksFerdigSafePaymentInfoView: FiksFerdigInfoBaseView {
     private let viewModel: FiksFerdigSafePaymentInfoViewModel
     private let simpleIndicatorProvider = SimpleTimeLineIndicatorProvider(font: .caption)
-
-    private lazy var headerIcon: UIImageView = {
-        let imageView = UIImageView(withAutoLayout: true)
-        imageView.contentMode = .scaleAspectFit
-        imageView.tintColor = .textPrimary
-        imageView.image = viewModel.icon
-        return imageView
-    }()
-
-    private lazy var headerTitle: Label = {
-        let label = Label(style: .bodyStrong)
-        label.numberOfLines = 0
-        label.text = viewModel.title
-        return label
-    }()
 
     private lazy var timeLineView: TimeLineView = {
         let timeLineView = TimeLineView(
@@ -31,21 +16,9 @@ public final class FiksFerdigSafePaymentInfoView: UIView {
         return timeLineView
     }()
 
-    private lazy var contentView: UIStackView = {
-        return UIStackView(axis: .vertical, spacing: .spacingM, alignment: .leading)
-    }()
-
-    private lazy var titleView: UIStackView = {
-        return UIStackView(axis: .horizontal, spacing: .spacingM, alignment: .leading)
-    }()
-
-    public init(
-        viewModel: FiksFerdigSafePaymentInfoViewModel,
-        withAutoLayout: Bool = false
-    ) {
+    public init(viewModel: FiksFerdigSafePaymentInfoViewModel, withAutoLayout: Bool = false) {
         self.viewModel = viewModel
-        super.init(frame: .zero)
-        translatesAutoresizingMaskIntoConstraints = !withAutoLayout
+        super.init(viewModel: viewModel.headerViewModel, withAutolayout: withAutoLayout)
         setup()
     }
 
@@ -54,17 +27,6 @@ public final class FiksFerdigSafePaymentInfoView: UIView {
     }
 
     private func setup() {
-        titleView.addArrangedSubviews([
-            headerIcon,
-            headerTitle
-        ])
-
-        contentView.addArrangedSubviews([
-            titleView,
-            timeLineView
-        ])
-
-        addSubview(contentView)
-        contentView.fillInSuperview()
+        addViewToContentView(timeLineView)
     }
 }
--- Sources/Components/Fiks Ferdig/FiksFerdigInfoView/FiksFerdigSafePaymentInfoViewModel.swift
+++ Sources/Components/Fiks Ferdig/FiksFerdigInfoView/FiksFerdigSafePaymentInfoViewModel.swift
@@ -1,17 +1,17 @@
 import UIKit
 
 public final class FiksFerdigSafePaymentInfoViewModel {
-    public let title: String
-    public let icon: UIImage
     public let timeLineItems: [TimeLineItem]
+    public let headerViewModel: FiksFerdigInfoBaseViewModel
 
     public init(
         headerTitle: String,
-        icon: UIImage? = nil,
         timeLineItems: [TimeLineItem]
     ) {
-        self.title = headerTitle
-        self.icon = icon ?? UIImage(named: .tjtLockShield)
         self.timeLineItems = timeLineItems
+        self.headerViewModel = FiksFerdigInfoBaseViewModel(
+            title: headerTitle,
+            icon: UIImage(named: .tjtLockShield)
+        )
     }
 }
