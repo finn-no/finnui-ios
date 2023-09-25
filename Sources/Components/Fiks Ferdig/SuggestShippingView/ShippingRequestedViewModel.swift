305f632bf3000d30efc9026f38417cfb8fa430c9
Attila Szilagyi <attila.szilagyi@schibsted.com>
2023-09-25 17:12:30 +0200

    Remove TJT related components.

--- Sources/Components/Fiks Ferdig/SuggestShippingView/ShippingRequestedViewModel.swift
+++
@@ -1,9 +0,0 @@
-public struct ShippingRequestedViewModel {
-    let title: String
-    let message: String
-
-    public init(title: String, message: String) {
-        self.title = title
-        self.message = message
-    }
-}
